153db4831d046cc47a6eaff54394b0923e425998
KwonHyeokGeon <weekend12@icloud.com>
2023-06-09 16:41:02 +0900

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,126 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
+const methodOverride = require('method-override');
 
-app.listen(5000, function(){
-  console.log('server is open');
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
+
+let db;
+let save;
+
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect("mongodb+srv://admin:qwer1234@cluster0.6pc4fpp.mongodb.net/", function (err, client) {
+  if (err) { return console.log(err) }
+
+  db = client.db('sample_mflix');
+  save = client.db('test2')
+
+
+
+  app.listen(5000, function () {
+    console.log('server is open');
+  })
 })
 
-app.use(express.static(path.join(__dirname, 'Ottugi/dist')))
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'Ottugi/dist/index.html'))
+app.get('/write', function (req, res) {
+  res.render('write.ejs')
 })
+
+app.post('/newpost', function (req, res) {
+  console.log(req.body);
+
+  const data = {
+    name: req.body.name,
+    age: req.body.age
+  }
+
+  save.collection('test2').insertOne(data, function (err, result) {
+    res.redirect('/list');
+  })
+})
+
+app.get('/list', function (req, res) {
+  save.collection('test2').find().toArray(function (err, result) {
+    res.render('list.ejs', { data: result })
+  })
+})
+// 해당 앱의 주소가 list로 접근했다면, test2데이터베이스에서 모든 문서를 찾고 그 문서를 배열로 만든 뒤 list.ejs로 데이터를 넘긴다.
+
+const ObjectId = require('mongodb').ObjectId
+
+app.delete('/delete', function (req, res) {
+
+  save.collection('test2').deleteOne({
+    _id: ObjectId(req.body.id)
+  }, function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/list')
+  })
+})
+
+app.post('/edit/:id', function (req, res) {
+  save.collection('test2').findOne({ _id: ObjectId(req.params.id) }, function (err, result) {
+    res.render('edit.ejs', { data: result })
+  })
+})
+app.put('/edit', function (req, res) {
+  console.log(req.body.name, req.body.age);
+  save.collection('test2').updateOne({ _id: ObjectId(req.body.id) }, {}, {
+    $set: {
+      name: req.body.name,
+      age: req.body.age
+    }
+  }, function (err, result) {
+    res.redirect('/list')
+  })
+})
+app.get('/search', function (req, res) {
+  save.collection('test2').find({ name: req.query.value }).toArray(function (err, result) {
+    console.log(result.length);
+    if (result.length > 0) {
+      // 구조분해할당
+      // 배열 첫번째 요소는 선언한 변수에 값을 넣고 나머지 모든 요소를 새 배열로 할당한다.
+      const [list, ...lists] = result;
+
+      res.render('search.ejs', { data: list })
+    } else {
+      res.send('no data')
+    }
+  })
+})
+
+app.get('/api/movie', function (req, res) {
+  db.collection('movies').find().toArray(function (err, result) {
+    const data2 = JSON.stringify(result, null, 2);
+    res.render('api.ejs', { data: data2 })
+  })
+})
+
+app.get('/api/movie/:year', function (req, res) {
+  const category = Number(req.params.year) 
+  db.collection('movies').find({ year: category }).toArray(function (err, result) {
+    const data2 = JSON.stringify(result, null, 2);
+    res.render('api.ejs', { data: data2 })
+  })
+})
+app.get('/api/movie/:year:countrie', function (req, res) {
+  const category = Number(req.params.year) ?? 0
+  const countrie = req.params.countrie.toUpperCase();
+  db.collection('movies').find({ year: category, countries: countrie }).toArray(function (err, result) {
+    const data2 = JSON.stringify(result, null, 2);
+    res.render('api.ejs', { data: data2 })
+  })
+})
+
+// app.use(express.static(path.join(__dirname, 'Ottugi/dist')))
+// app.get('/', function(req, res){
+//   res.sendFile(path.join(__dirname, 'Ottugi/dist/index.html'))
+// })
 
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html')
@@ -23,7 +134,8 @@ app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/aboutquestion.html')
 // })
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'Ottugi/dist/index.html'))
 })
 module.exports = app;
+
